d737da26f40f32c859b67c3882e5f88e39576d78
Charish128 <charis12876@gmail.com>
2025-02-16 00:30:51 +0700

    feat(surat-keluar): implement surat keluar page

--- e-filing/src/pages/SuratKeluar.tsx
+++ e-filing/src/pages/SuratKeluar.tsx
@@ -1,8 +1,87 @@
 import React from 'react';
-// import 'index.css';
-import { Breadcrumb, Layout, theme } from 'antd';
+import { Breadcrumb, Layout, theme, Space, Table, Tag } from 'antd';
+import type { TableProps } from 'antd';
+
+interface DataType {
+  key: string;
+  nomorSurat: string;
+  tanggalSurat: string;
+  perihal: string;
+  tujuanSurat: string;
+  tags: string[];
+}
 
 const { Content } = Layout;
+
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Nomor Surat',
+    dataIndex: 'nomorSurat',
+    key: 'nomorSurat',
+  },
+  {
+    title: 'Tanggal Surat',
+    dataIndex: 'tanggalSurat',
+    key: 'tanggalSurat',
+  },
+  {
+    title: 'Perihal',
+    dataIndex: 'perihal',
+    key: 'perihal',
+  },
+  {
+    title: 'Tujuan Surat',
+    key: 'tujuanSurat',
+    dataIndex: 'tujuanSurat',
+  },
+  {
+    title: 'Kelola',
+    key: 'kelola',
+    render: (_, { tags }, record) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    nomorSurat: 'H/UBL/LAB/010/12/01/24',
+    tanggalSurat: "31/01/2024",
+    perihal: 'Pemberitahuan Ketersediaan Laboratorium ICT',
+    tujuanSurat: 'Lab ICT',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    nomorSurat: 'H/UBL/LAB/010/12/01/24',
+    tanggalSurat: "31/01/2024",
+    perihal: 'Pemberitahuan Ketersediaan Laboratorium ICT',
+    tujuanSurat: 'Lab ICT',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '3',
+    nomorSurat: 'H/UBL/LAB/010/12/01/24',
+    tanggalSurat: "31/01/2024",
+    perihal: 'Pemberitahuan Ketersediaan Laboratorium ICT',
+    tujuanSurat: 'Lab ICT',
+    tags: ['nice', 'developer'],
+  },
+];
 
 const SuratKeluar: React.FC = () => {
   const {
@@ -11,9 +90,7 @@ const SuratKeluar: React.FC = () => {
 
   return (
     <Content style={{ margin: '0 16px' }}>
-      <Breadcrumb items={[{ title: 'sample' }]} style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>User</Breadcrumb.Item>
-        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+      <Breadcrumb items={[{ title: 'Surat Keluar' }]} style={{ margin: '16px 0', fontSize: '30px', fontWeight: 'bold' }}>
       </Breadcrumb>
       <div
         style={{
@@ -23,7 +100,7 @@ const SuratKeluar: React.FC = () => {
           borderRadius: borderRadiusLG,
         }}
       >
-        Bill is a cat.
+        <Table<DataType> columns={columns} dataSource={data} />;
       </div>
     </Content>
   );
